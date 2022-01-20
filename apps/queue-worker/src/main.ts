598eb2b3ac268b5f1b46aef7175666b32a506450
Israel <israel.hmis@gmail.com>
2022-01-20 11:22:16 +0100

    change server configuration

--- apps/queue-worker/src/main.ts
+++ apps/queue-worker/src/main.ts
@@ -3,6 +3,6 @@ import { QueueWorkerModule } from './queue-worker.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(QueueWorkerModule);
-  await app.listen(3000);
+  await app.listen(3901);
 }
 bootstrap();
